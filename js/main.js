0a21b12a72fc4f37c6facd6be7c6343a898ddd09
Nishant. <98388538+NishantKB@users.noreply.github.com>
2024-04-18 22:16:24 +0545

    completed index file

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const galleryItems = document.querySelectorAll('.galleryItem');
+const lightbox = document.querySelector('.lightbox');
+const lightboxImage = document.querySelector('.lightboxImage');
+const closeBtn = document.querySelector('.close');
+
+galleryItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const imgSrc = item.querySelector('img').src;
+        lightboxImage.src = imgSrc;
+        lightbox.style.display = 'block';
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+// function sendMail() {
+//     let params = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         query: document.getElementById("query").value
+//     };
+
+//     emailjs.send('service_5ky7acm', 'template_gft9dsh', params).then(function (res) {
+//         alert("Email Sent Successfully.");
+//     }).catch(function (error) {
+//         alert("Failed to send email. Error: " + error);
+//     });
+// }
